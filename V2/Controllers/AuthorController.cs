fd0137d9a0d034ea193bcefa44d4214b8eba0d1d
Celielton Ramalho <celton1712@hotmail.com>
2021-10-13 15:52:10 -0300

    versioning

--- V2/Controllers/AuthorController.cs
+++ V2/Controllers/AuthorController.cs
@@ -1,8 +1,4 @@
-﻿using AutoMapper;
-using library_api.Infrastructure.Repository;
-using library_api.Infrastructure.UnitOfWork;
-using library_api.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace library_api.V2.Controllers
